1c2d8ba3ceb1e1ed67fd1d7d1e9b86e49946823f
davrob01 <davrob01@gmail.com>
2016-02-14 21:17:06 -0600

    label for time

--- AmazonBestSellers/Form1.Designer.cs
+++ AmazonBestSellers/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblTimeValue = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnStart
@@ -41,15 +45,45 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.lblTimeValue);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 126);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(3, 16);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(83, 13);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Execution Time:";
+            // 
+            // lblTimeValue
+            // 
+            this.lblTimeValue.AutoSize = true;
+            this.lblTimeValue.Location = new System.Drawing.Point(92, 16);
+            this.lblTimeValue.Name = "lblTimeValue";
+            this.lblTimeValue.Size = new System.Drawing.Size(0, 13);
+            this.lblTimeValue.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +91,9 @@
         #endregion
 
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTimeValue;
+        private System.Windows.Forms.Label lblTime;
     }
 }
 
--- AmazonBestSellers/Form1.cs
+++ AmazonBestSellers/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace AmazonBestSellers
 {
@@ -25,13 +26,20 @@ namespace AmazonBestSellers
 
         private async void btnStart_Click(object sender, EventArgs e)
         {
+            btnStart.Enabled = false;
+            var watch = Stopwatch.StartNew();
             await StartScrape();
+            long time = watch.ElapsedMilliseconds / 1000;
+
+            lblTimeValue.Text = time.ToString();
+            btnStart.Enabled = true;
         }
 
         private async Task StartScrape()
         {
             //string url = "http://www.amazon.com/gp/bestsellers/books";
-            string url = "http://www.amazon.com/Best-Sellers-Books-Architectural-Buildings/zgbs/books/266162/ref=zg_bs_nav_b_3_173508";
+            string url = "http://www.amazon.com/Best-Sellers-Books-Arts-Photography/zgbs/books/1/ref=zg_bs_unv_b_2_173508_1";
+            //string url = "http://www.amazon.com/Best-Sellers-Books-Architectural-Buildings/zgbs/books/266162/ref=zg_bs_nav_b_3_173508";
             Domain amazonUS = new Domain(url, "Books");
 
             await amazonUS.ProcessCategory();
@@ -40,11 +48,12 @@ namespace AmazonBestSellers
 
             foreach (Category category in amazonUS.Categories.OrderBy(x => x.Name))
             {
+                strBuilder.AppendLine();
                 strBuilder.AppendLine(category.Name);
                 strBuilder.AppendLine();
                 foreach (Book book in category.Books.OrderBy(x => x.Rank))
                 {
-                    strBuilder.AppendFormat("{0} - {1} - {2}", category.Name, book.Rank, book.ISBN);
+                    strBuilder.AppendFormat("{0} - {1} - {2} - {3}", category.Name, book.Rank, book.ISBN, book.Title);
                     strBuilder.AppendLine();
                 }
             }
