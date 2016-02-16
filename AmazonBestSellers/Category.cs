8b25d2c37b80ca809316625211dd07f90c9206ad
davrob01 <davrob01@gmail.com>
2016-02-15 19:17:56 -0600

    Added try catch and specific log message

--- AmazonBestSellers/Category.cs
+++ AmazonBestSellers/Category.cs
@@ -22,87 +22,99 @@ namespace AmazonBestSellers
 
         public async Task<List<Category>> RetrieveCategoryData(int qAboveFold, int qPage)
         {
-            string url = string.Format("{0}?_encoding=UTF8&pg={1}&ajax=1&isAboveTheFold={2}", URL, qPage, qAboveFold);
-
-            string html = await DownloadHtmlPage(url);
-
             List<Category> subCategories = new List<Category>();
-
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-
-            doc.LoadHtml(html);
-            var root = doc.DocumentNode;
-            var titleNodes = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("zg_title"));
-
-            int rank = ((qPage - 1) * 20) + 1;
-
-            if(qAboveFold == 0)
+            try
             {
-                rank += 3; // there are only 3 items on the first ajax page, hopefully that is true for every category
-                // consider using the rank number field on the html page
+                string url = string.Format("{0}?_encoding=UTF8&pg={1}&ajax=1&isAboveTheFold={2}", URL, qPage, qAboveFold);
+
+                string html = await DownloadHtmlPage(url);
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+                doc.LoadHtml(html);
+                var root = doc.DocumentNode;
+                var titleNodes = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("zg_title"));
+
+                int rank = ((qPage - 1) * 20) + 1;
+
+                if(qAboveFold == 0)
+                {
+                    rank += 3; // there are only 3 items on the first ajax page, hopefully that is true for every category
+                    // consider using the rank number field on the html page
+                }
+
+                foreach (HtmlNode node in titleNodes)
+                {
+                    string link = node.FirstChild.GetAttributeValue("href", "").Trim();
+                    string ISBN = link.Split(new string[] { "/dp/" }, StringSplitOptions.None)[1].Split(new string[] { "/" }, StringSplitOptions.None)[0];
+                    string title = node.FirstChild.InnerText;
+                    Book book = new Book(rank, title, ISBN, link);
+
+                    Books.Add(book);
+                    Counter.BooksAdded++;
+
+                    rank++;
+                }
             }
-
-            foreach (HtmlNode node in titleNodes)
+            catch (Exception ex)
             {
-                string link = node.FirstChild.GetAttributeValue("href", "").Trim();
-                string ISBN = link.Split(new string[] { "/dp/" }, StringSplitOptions.None)[1].Split(new string[] { "/" }, StringSplitOptions.None)[0];
-                string title = node.FirstChild.InnerText;
-                Book book = new Book(rank, title, ISBN, link);
-
-                Books.Add(book);
-                Counter.BooksAdded++;
-
-                rank++;
+                Logger.Log(string.Format("Failed to process page {0} of URL: {1}", qPage, URL), ex);
             }
 
             return subCategories;
         }
         public async Task<List<Category>> RetrieveCategoryData(int qPage)
         {
-            string url = string.Format("{0}?pg={1}", URL, qPage);
-
-            string html = await DownloadHtmlPage(url);
-
             List<Category> subCategories = new List<Category>();
-
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-
-            doc.LoadHtml(html);
-            var root = doc.DocumentNode;
-            var titleNodes = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("zg_title"));
-
-            int rank = 1;
-
-            foreach (HtmlNode node in titleNodes)
+            try
             {
-                string link = node.FirstChild.GetAttributeValue("href", "").Trim();
-                string ISBN = link.Split(new string[] { "/dp/" }, StringSplitOptions.None)[1].Split(new string[] { "/" }, StringSplitOptions.None)[0];
-                string title = node.FirstChild.InnerText;
-                Book book = new Book(rank, title, ISBN, link);
+                string url = string.Format("{0}?pg={1}", URL, qPage);
 
-                Books.Add(book);
-                Counter.BooksAdded++;
+                string html = await DownloadHtmlPage(url);
 
-                rank++;
-            }
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
 
-            HtmlNode categoryElement = doc.GetElementbyId("zg_browseRoot");
+                doc.LoadHtml(html);
+                var root = doc.DocumentNode;
+                var titleNodes = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("zg_title"));
 
-            HtmlNode lastUlElement = categoryElement.Descendants("ul").Last();
+                int rank = 1;
 
-            bool hasSubCategories = ! lastUlElement.Descendants().Any(n => n.GetAttributeValue("class", "").Equals("zg_selected"));
-
-            if(hasSubCategories)
-            {
-                IEnumerable<HtmlNode> aElements = lastUlElement.Descendants().Where(n => n.OriginalName == "a");
-
-                foreach(HtmlNode aElement in aElements)
+                foreach (HtmlNode node in titleNodes)
                 {
-                    string link = aElement.GetAttributeValue("href", "").Trim();
-                    string name = string.Format("{0} > {1}", Name, aElement.InnerText);
-                    Category category = new Category(name, link);
-                    subCategories.Add(category);
+                    string link = node.FirstChild.GetAttributeValue("href", "").Trim();
+                    string ISBN = link.Split(new string[] { "/dp/" }, StringSplitOptions.None)[1].Split(new string[] { "/" }, StringSplitOptions.None)[0];
+                    string title = node.FirstChild.InnerText;
+                    Book book = new Book(rank, title, ISBN, link);
+
+                    Books.Add(book);
+                    Counter.BooksAdded++;
+
+                    rank++;
                 }
+
+                HtmlNode categoryElement = doc.GetElementbyId("zg_browseRoot");
+
+                HtmlNode lastUlElement = categoryElement.Descendants("ul").Last();
+
+                bool hasSubCategories = !lastUlElement.Descendants().Any(n => n.GetAttributeValue("class", "").Equals("zg_selected"));
+
+                if (hasSubCategories)
+                {
+                    IEnumerable<HtmlNode> aElements = lastUlElement.Descendants().Where(n => n.OriginalName == "a");
+
+                    foreach (HtmlNode aElement in aElements)
+                    {
+                        string link = aElement.GetAttributeValue("href", "").Trim();
+                        string name = string.Format("{0} > {1}", Name, aElement.InnerText);
+                        Category category = new Category(name, link);
+                        subCategories.Add(category);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Logger.Log(string.Format("Failed to process page {0} of URL: {1}", qPage, URL), ex);
             }
 
             return subCategories;
--- AmazonBestSellers/Logger.cs
+++ AmazonBestSellers/Logger.cs
@@ -15,6 +15,18 @@ namespace AmazonBestSellers
             File.WriteAllText("log.txt", string.Format("****** Log for {0} {1} *******", datetime.ToLongDateString(), datetime.ToLongTimeString()));
         }
 
+        public static void Log(string message, Exception ex)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.AppendLine();
+            strBuilder.AppendLine(message);
+            strBuilder.AppendLine(ex.GetType().Name);
+            strBuilder.AppendLine(ex.Message);
+            strBuilder.AppendLine(ex.StackTrace);
+
+            File.AppendAllText("log.txt", strBuilder.ToString());
+        }
+
         public static void Log(Exception ex)
         {
             StringBuilder strBuilder = new StringBuilder();
