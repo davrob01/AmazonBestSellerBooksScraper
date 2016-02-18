6d0d9a6be43e8e0b81431e53505472859adce59e
davrob01 <davrob01@gmail.com>
2016-02-18 15:15:24 -0600

    Only one instance can be running at  a time

--- AmazonBestSellers/Program.cs
+++ AmazonBestSellers/Program.cs
@@ -14,9 +14,20 @@ namespace AmazonBestSellers
         [STAThread]
         static void Main()
         {
+            bool result;
+            var mutex = new System.Threading.Mutex(true, "D-ROB Software/ AmazonBestSellers", out result);
+
+            if (!result)
+            {
+                MessageBox.Show("Another instance is already running.");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            GC.KeepAlive(mutex);                // mutex shouldn't be released - important line
         }
     }
 }
